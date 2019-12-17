5c82a9311cbc52208fdfb34236cdadb40dea89ad
Otto Cedeno <otto@ottostacos.com>
2019-12-17 00:21:12 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_reader :sides
+
+  def initialize(side_one, side_two, side_three)
+    @sides = [side_one, side_two, side_three]
+  end
+
+  def zero_or_negative
+    sides.any? {|side| side <= 0}
+  end
+
+  def triangle_inequality?
+    sides[0] + sides[1] <= sides[2] || sides[1] + sides[2] <= sides[0] || sides[0] + sides[2] <= sides[1]
+  end
+
+  def kind
+    raise TriangleError if self.zero_or_negative || self.triangle_inequality?
+
+    if sides.uniq.size == 1
+      :equilateral
+    elsif sides.uniq.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+# binding.pry
